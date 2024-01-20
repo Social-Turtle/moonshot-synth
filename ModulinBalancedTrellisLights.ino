c8321a8777f1f64055ca7264bb1058daa2238049
Social Turtle <52430191+Social-Turtle@users.noreply.github.com>
2024-01-20 15:32:21 -0800

    DisplayWorking!

--- ModulinBalancedTrellisLights.ino
+++ ModulinBalancedTrellisLights.ino
@@ -1,21 +1,31 @@
-/* 
+/*
 TODO:
- - Develop function to make a gradient slide effect from notes + bends - start with computeNote
- - Develop a single-press input function on trellis for settings panel - need to determine what settings we want to mess with.
- - Attain multi-channel midi output via drum machine
- - Reformat toDisplay for new display module
- - Sometimes launches at G9 (MIDI 127) why? - - add an if-cap to highest note frequency
+ - Develop function to make a gradient slide effect from notes + bends - start with computeNote - - Give this to a MoonShotter
+ - Sometimes launches at G9 (MIDI 127) why? - signal noise issue with potentiometer. Let's solve that.
+ - Chord pack development
+ - general smoothness/bug hunting
+ - accellerometer interfacing
+ - led strip debugging
  */
+// Primary Synth operates on Channel 0
+// Drum Kit operates on Channel 1
+// Chord Library/Arpeggiator operates on Channel 2
 
 // LIBRARIES USED
 #include <Adafruit_NeoPixel.h>
 #include <MIDIUSB.h> //Midi Communication protocol
 #include <Wire.h> // i2c protocol
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include "Adafruit_NeoTrellis.h" // Neotrellis library
 #define Y_DIM 4 //number of rows of key
 #define X_DIM 8 //number of columns of keys
 #define LED_PIN     2    // Define the data pin connected to the LED strip
 #define NUM_LEDS    29   // Define the number of LEDs in your strip
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+// accellerometer sits at 0x62
+// display @ 0x3C
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -26,6 +36,7 @@ Adafruit_MultiTrellis trellis((Adafruit_NeoTrellis *)t_array, Y_DIM/4, X_DIM/4);
 // PIN ASSIGNMENTS
 const int velocityPin = A1; // analog input for velocity potentiometer
 const int pitchPin = A0; // the number of the ribbon potentiometer pin
+const int bendPin = A2;
 
 const unsigned long debounceDelay = 5; //length in ms
 const unsigned long debounceNote = 50;
@@ -44,6 +55,7 @@ byte pentatonic[5] = {0, 2, 4, 7, 9};
 int numModes = 4; 
 byte modeLengths[4] = {7, 7, 6, 5};
 byte* modes[] = {major, minor, blues, pentatonic}; 
+int bendVal;
 
 // INITIALIZE AT GLOBAL
 unsigned long lastButtonDebounceTime = 0;
@@ -64,13 +76,25 @@ int readIndex = 0;
 int total = 0;
 int noteAverage = 0;
 
+int bendAverage;
+int bendTotal;
+const int bendPoints = 32;
+int bendReadings[bendPoints];
+int bendIndex = 0;
+
 boolean averagingPaused = false;
 unsigned long lastDebounceTime = 0;
+
+unsigned long tempo; // store tempo value
+unsigned long lastTempoPressTime = 0; // store more recent tempo press
+unsigned long nextBeat = 0;
+int beatIndex = 0;
+bool dontbother = false;
 ///////////////////////////////////////////////////////////////////
 
 // TRELLIS DATA
 byte modeMemory[4][32] = { // Button position memory bank
-  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // store drum machine data
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
@@ -93,21 +117,43 @@ TrellisCallback blink(keyEvent evt){ // Operational Trellis FSM
     if (evt.bit.NUM >= 0 && evt.bit.NUM <= 2 && evt.bit.NUM != currentPage) { // If we press a novel page button, update Trellis Page
         currentPage = evt.bit.NUM;
         trellisFlicker(currentPage);
-    } else if (currentPage == 2 && evt.bit.NUM >= 4 && evt.bit.NUM <= 31) { // a standard button in settings mode
+    } else if (currentPage == 2 && evt.bit.NUM >= 4 && evt.bit.NUM <= 31) { // CONTROL SETTINGS FROM HERE:
         if (evt.bit.NUM == 8) { // Row 1, button 0 = - 1 semitone
           startNote--;
+          trellis.setPixelColor(evt.bit.NUM, 0x550000); // RED
         } else if (evt.bit.NUM == 9) { // Row 1, button 1 = + 1 semitone
-          startNote++;          
+          startNote++;
+          trellis.setPixelColor(evt.bit.NUM, 0x005500); // GREEN   
         } else if (evt.bit.NUM == 10) { // Row 1, button 2 = - 1 octave
-          startNote -= 12;          
+          startNote -= 12;
+          trellis.setPixelColor(evt.bit.NUM, 0x000055); // BLUE 
         } else if (evt.bit.NUM == 11) { // Row 1, button 3 = + 1 octave
-          startNote += 12;          
-        } 
-        // determine settings operations 
-    } else if (evt.bit.NUM >= 4 && evt.bit.NUM <= 31) { // if a standard button and chords or beatbox
+          startNote += 12;
+          trellis.setPixelColor(evt.bit.NUM, 0x550055); // PURPLE  
+        } else if (evt.bit.NUM == 12) { // Row 1, button 4 = MODECODE - 1 IF AVAILABLE
+          if (modeCode > 0) {
+            modeCode--;
+          }
+          Serial.println(modeCode);
+          trellis.setPixelColor(evt.bit.NUM, 0x005555); // CYAN
+        } else if (evt.bit.NUM == 13) { // Row 1, button 5 = MODECODE + 1 IF AVAILABLE
+          if (modeCode < 4) {
+            modeCode++;
+          }
+          Serial.println(modeCode);
+          trellis.setPixelColor(evt.bit.NUM, 0x552200); // ORANGE      
+        }
+    } else if (evt.bit.NUM > 3 && evt.bit.NUM <= 31) { // if a standard button and chords or beatbox
         toggleArray(evt.bit.NUM, currentPage);
+    } else if (evt.bit.NUM == 3) { // Tempo button!
+      dontbother = true;
+      tempo = millis() - lastTempoPressTime;
+      Serial.println(tempo);
+      lastTempoPressTime = millis();
+      nextBeat = lastTempoPressTime;
+      beatIndex = 0;
     }
-    trellis.show();      
+    trellis.show();
   }
     // WHEN A BUTTON HAS BEEN RELEASED
   else if(evt.bit.EDGE == SEESAW_KEYPAD_EDGE_FALLING) {
@@ -115,18 +161,15 @@ TrellisCallback blink(keyEvent evt){ // Operational Trellis FSM
       if (evt.bit.NUM == 0) { // set tab colors to page 0 - red
           trellis.setPixelColor(evt.bit.NUM, 0xFF0000);
           trellis.setPixelColor(1, 0xFFFF4B);
-          trellis.setPixelColor(2, 0x4BFF4B);
-          
+          trellis.setPixelColor(2, 0x4BFF4B);    
       } else if (evt.bit.NUM == 1) { // set tab colors to page 1 - yellow
           trellis.setPixelColor(evt.bit.NUM, 0xFFFF00);
           trellis.setPixelColor(0, 0xFF4B4B);
           trellis.setPixelColor(2, 0x4BFF4B);
-          
       } else if (evt.bit.NUM == 2) { // set tab colors to page 2 - green
           trellis.setPixelColor(evt.bit.NUM, 0x00FF00);
           trellis.setPixelColor(0, 0xFF4B4B);
           trellis.setPixelColor(1, 0xFFFF4B);
-          
       } else if (evt.bit.NUM == 3) { // if it's the tempo button, just keep it blue
           trellis.setPixelColor(evt.bit.NUM, 0x0000FF);
       }
@@ -165,10 +208,13 @@ void trellisFlicker(int currentPage) {
     }
 }
 
-void toDisplay(int note) {  // Send 3-digit integer display code to Metro
-  Wire.beginTransmission(10); // metro address
-  Wire.write((uint8_t*)&note, sizeof(note));
-  Wire.endTransmission();
+void toDisplay(String text) {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(text);
+  display.display();
 }
 
 // activate a MIDI note
@@ -176,6 +222,7 @@ void noteOn(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOn = { 0x09, 0x90 | channel, pitch, velocity };
   MidiUSB.sendMIDI(noteOn);
 }
+
 // turn off MIDI note
 void noteOff(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOff = { 0x08, 0x80 | channel, pitch, velocity };
@@ -204,7 +251,6 @@ void controlChange(byte channel, byte control, byte value) {
   MidiUSB.sendMIDI(event);
 }
 
-// Yet unused theoretical pitch bending function
 void pitchBend(byte channel, int value) {
   byte lowValue = value & 0x7F;
   byte highValue = value >> 7;
@@ -232,17 +278,17 @@ void clearData() {
   noteAverage = 0;
 }
 
-void updateNote(int pitch) {
+void updateNote(int pitch) { // add a check for 127 spikes
   if (pitch >= startNote) { // weed pitch errors
     if (pitch != lastNote) { // if note is different
       noteOff(0, lastNote, 0); //Kill the note
-      Serial.println("note is different");
+      //Serial.println("note is different");
       noteOn(0, pitch, noteVelocity); // trigger note
       isOn = 1;
       lastRibbonTriggerTime = millis();
       MidiUSB.flush();
       lastNote = pitch; // record note
-      toDisplay(pitch); // display on Metro
+      toDisplay("Diff Note!"); // display on Metro
     }
   }
 }
@@ -252,6 +298,7 @@ void updateNote(int pitch) {
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   Wire.setClock(50000); // match Metro Mini i2c clock of 50kHz
   
   Serial.println("Setup Begin");
@@ -304,74 +351,99 @@ void setup() {
 
 
 void loop() {
+  //toDisplay("Yo Mama");
+  if (millis() - nextBeat >= tempo/8 && dontbother) { // if we've reached one eighth
+    nextBeat = millis();
+    Serial.println(beatIndex);
+    if (modeMemory[0][8 + beatIndex] == 1) {
+      Serial.println("Hat");
+      noteOn(1, 42, 60); // closed high hat
+    }
+    if (modeMemory[0][16 + beatIndex] == 1) {
+      Serial.println("Snare");
+      noteOn(1, 38, 60);
+    }
+    if (modeMemory[0][24 + beatIndex] == 1) {
+      Serial.println("Kick");
+      noteOn(1, 36, 60);
+    }
+    MidiUSB.flush();
+    beatIndex++;     // do stuff on the eighth
+    if (beatIndex > 7) { // update the index of the 1/8th we're on
+      beatIndex = 0; // trip back to zero if we step over 7
+    }
+  }
 
-  
-  noteVelocity = map(analogRead(velocityPin), 330, 690, 0, 128);
-  trellis.read();
+  noteVelocity = map(analogRead(velocityPin), 330, 690, 0, 128);   // Convert velocitypin into a velocity value
+  trellis.read(); // Check the trellis
 
   if ((millis() - lastButtonDebounceTime) > debounceDelay) { // if it's been 5 ms since the last update
-    //int reading = digitalRead(buttonPin); // read the state of the pushbutton value:
-    if (noteVelocity != lastVelocity) {
-      lastButtonDebounceTime = millis(); // record when the button value changes
-    }
-    if (lastVelocity != noteVelocity) { // update current button state
+    if (noteVelocity != lastVelocity) { // if the velocity has changed, record when
+      lastButtonDebounceTime = millis();
       lastVelocity = noteVelocity;
-      } 
-      if (noteVelocity < toggleThreshold && isOn == 1) { // If the button is released but a note is playing: 
-        //Serial.println("button up, kill note");
+    } 
+      if (noteVelocity < toggleThreshold && isOn == 1) { // if the string is released but a note is playing: 
         noteOff(0, lastNote, 0); //Kill the note
         MidiUSB.flush();
-        toDisplay(999); // turn off display if no note is present
+        toDisplay(""); // turn off display if no note is present
         isOn = 0;
-
-      } else if (noteVelocity > toggleThreshold && isOn == 0) { // the button is pressed and no note is playing
-        //Serial.println("button down, play once");
+    } else if (noteVelocity > toggleThreshold && isOn == 0) { // the string is pressed and no note is playing
         noteOn(0, computeNote(modeCode, pitchPin, fretNumber), noteVelocity); //play the note
         lastRibbonTriggerTime = millis();
         MidiUSB.flush();
-        toDisplay(computeNote(modeCode, pitchPin, fretNumber)); // turn off display if no note is present
+        toDisplay("Playing");
         isOn = 1;
-      }
     }
+  }
 
-  if (noteVelocity > toggleThreshold) {
+  if (noteVelocity > toggleThreshold) { // what case is this??? - - - - - - - - - - - -  - - -  - **
     if (millis() - lastRibbonTriggerTime > debounceNote) {
       updateNote(computeNote(modeCode, pitchPin, fretNumber));
     }
   }
     lastVelocity = noteVelocity;
-    
-    if (noteVelocity < toggleThreshold) { // Button is pressed
-      if (!averagingPaused) {
-        averagingPaused = true;
-        //Serial.println("Averaging Paused");
-      }
-    } else { // Button is released
-      if (averagingPaused) {
-        clearData();
-        averagingPaused = false;
-        //Serial.println("Averaging Resumed");
-      }
+  if (noteVelocity < toggleThreshold) { // note is triggered
+    if (!averagingPaused) {
+      averagingPaused = true;
+      //Serial.println("Averaging Paused");
+    }
+  } else { // string released
+    if (averagingPaused) {
+      clearData();
+      averagingPaused = false;
+      //Serial.println("Averaging Resumed");
+    }
+  }
+
+  if (!averagingPaused) {
+    total = total - readings[readIndex];
+    readings[readIndex] = analogRead(pitchPin);
+    total = total + readings[readIndex];
+    readIndex++;
+    if (readIndex >= numReadings) {
+      readIndex = 0;
+    }
+    noteAverage = total / numReadings; // **skews data low at start. How to fix this?
+  }
+
+    // Running average for note bend data
+    bendTotal = bendTotal - bendReadings[bendIndex];
+    bendReadings[bendIndex] = analogRead(bendPin);
+    bendTotal = bendTotal + bendReadings[bendIndex];
+    bendIndex++;
+    if (bendIndex >= bendPoints) {
+      bendIndex = 0;
     }
 
-    if (!averagingPaused) {
-      total = total - readings[readIndex];
-      readings[readIndex] = analogRead(pitchPin);
-      total = total + readings[readIndex];
-      readIndex = readIndex + 1;
-
-      if (readIndex >= numReadings) {
-        readIndex = 0;
-      }
-
-      noteAverage = total / numReadings;
-      //Serial.println(noteAverage);
+    bendAverage = bendTotal / bendPoints;
+    bendVal = map(bendAverage, 2, 1020, 0, 16383);
+    if (bendVal > 8192 + 1500 || bendVal < 8192 - 1500) { // If pot is distant enough to trigger a bend . - - - - - - - - - - - - This methodology spams the pitch wheel, and could use to be updated to only send bend values when either 1: the note played changes or 2: the bend value changes outside of standard noise.
+      pitchBend(0, bendVal);
     }
 
   int sensorValue = analogRead(pitchPin);  // Read analog input value
   int numLEDsToLight = map(sensorValue, 330, 650, 0, NUM_LEDS);  // Map input value to the number of LEDs
-  Serial.print(numLEDsToLight);
-  Serial.println();
+  //Serial.println(numLEDsToLight);
   // Turn off all LEDs
   for (int i = 0; i < NUM_LEDS; i++) {
     strip.setPixelColor(i, 0);  // Set color to 'off' (Black)
@@ -383,5 +455,4 @@ void loop() {
     strip.setPixelColor(i, strip.Color(0, 0, 80));  // Set color to Red (adjust as needed)
   }
   strip.show();  // Update the strip to show the illuminated LEDs
-
 }
